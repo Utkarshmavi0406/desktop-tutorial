813bac9707cccf78327eca6e2f2c48028f56b55d
Utkarshmavi0406 <115769809+Utkarshmavi0406@users.noreply.github.com>
2023-03-08 14:37:57 +0530

    Update stream_1.py

--- stream_1.py
+++ stream_1.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import pickle as pkl
 import os
+os.path.join("")
 # os.chdir(r"C:\Users\utkar\OneDrive\Desktop\Project")
 # pip install bz2file
 # import bz2file as bz2
